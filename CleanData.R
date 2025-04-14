6ad9dbc9cb688458796c97abd30dd7ee3b2ad268
teresatrinh <teresa.trinh43@gmail.com>
2025-04-14 11:09:18 -0500

    removed dose equivalent column and normalized gamma channels

--- CleanData.R
+++ CleanData.R
@@ -15,7 +15,7 @@ data <- data %>%
     gamma5 = GAMMA.COUNT.RATE.R05..CPM.,
     gamma6 = GAMMA.COUNT.RATE.R06..CPM.,
     gamma7 = GAMMA.COUNT.RATE.R07..CPM.,
-    gamma8 = GAMMA.COUNT.RATE.R08..CPM., 
+    gamma8 = GAMMA.COUNT.RATE.R08..CPM.,
     gamma9 = GAMMA.COUNT.RATE.R09..CPM.,
     status = STATUS
   )
@@ -31,3 +31,25 @@ write.csv(data, "data/NE_LINCOLN_2008_2024.csv")
 noNa <- drop_na(data)
 
 write.csv(noNa, "data/NE_LINCOLN_NO_NA.csv")
+
+# remove dose equivalent column and remove any rows with NA in any of the gamma channels
+noDose <- data %>%
+  select(id, location, time, gamma2, gamma3, gamma4, gamma5, gamma6, gamma7, gamma8, gamma9, gammaSum, status) %>%
+  drop_na()
+
+write.csv(noDose, "data/NE_LINCOLN_NO_DOSE.csv")
+
+# normalize gamma channels
+normalize <- noDose %>%
+  mutate(
+    log_gamma2 = log(gamma2),
+    log_gamma3 = log(gamma3),
+    log_gamma4 = log(gamma4),
+    log_gamma5 = log(gamma5),
+    log_gamma6 = log(gamma6),
+    log_gamma7 = log(gamma7),
+    log_gamma8 = log(gamma8),
+    log_gamma9 = log(gamma9)
+  )
+
+write.csv(normalize, "data/NE_LINCOLN_NORMALIZED.csv")
